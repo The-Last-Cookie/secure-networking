6bcaa3510bef2dade45d201ea08b9d144161705c
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2024-02-28 14:22:30 +0100

    Add idea for report function

--- tools/scripts/scan_preparation_win.ps1
+++ tools/scripts/scan_preparation_win.ps1
@@ -2,20 +2,30 @@
 # requires -Version 5.0
 # As admin: powershell.exe -ExecutionPolicy RemoteSigned
 
-. .\UACModule.ps1
-. .\RemoteRegistryModule.ps1
+. .\Modules\UACModule.ps1
+. .\Modules\RemoteRegistryModule.ps1
+
+function Get-Report
+{
+	# create table from config file
+}
+
+function Start-Check
+{
+	Write-Host "This script will now go over every setting that is relevant for a Nessus scan."
+	Write-Host "Keep in mind that not setting every configuration to its required value may impact the scan.`n"
+
+	HandleUserAccountControl
+	#HandleRemoteRegistry
+
+	Write-Host "The system has been prepared for the scan."
+}
 
 Write-Host "Nessus scan readiness check"
 Write-Host "---------------------------`n"
 
 $LocalHost = [Environment]::MachineName
 $LocalDomain = (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
-Write-Host "Check executing for $LocalHost, part of $LocalDomain"
+Write-Host "Script executing for $LocalHost, part of $LocalDomain"
 
-Write-Host "This script will now go over every setting that is relevant for a Nessus scan."
-Write-Host "Keep in mind that not setting every configuration to its required value may impact the scan.`n"
-
-HandleUserAccountControl
-#EnableRemoteRegistry
-
-Write-Host "The system has been prepared for the scan."
+Start-Check
