e1d1f3ae84f6c407d10e779f983b6c7f4cc76473
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2024-07-21 11:59:47 +0200

    Use own UPS interface for display script

--- case/display/stats.py
+++ case/display/stats.py
@@ -22,10 +22,10 @@
 # THE SOFTWARE.
 
 import time
-import smbus2
-import logging
-from ina219 import INA219,DeviceRangeError
 
+
+# https://pypi.org/project/Adafruit-SSD1306/
+# https://pypi.org/project/adafruit-circuitpython-ssd1306/
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
@@ -35,46 +35,59 @@ from PIL import ImageFont
 
 import subprocess
 
+from ups_device import Bus, Supply, Battery
+
+
+class Screen:
+    def __init__(self) -> None:
+        self._adafruit = Adafruit_SSD1306.SSD1306_128_64(rst=None)
+        self._adafruit.begin()
+        self._adafruit.clear()
+        self._adafruit.display()
+
+        self.width = self._adafruit.width
+        self.height = self._adafruit.height
+        self.font = ImageFont.truetype('PixelOperator.ttf', 16)
+
+        # Make sure to create image with mode '1' for 1-bit color.
+        self._image = Image.new('1', (self.width, self.height)) # stellt den Rahmen (Frame) dar, in dem alles gezeichnet wird
+        self._draw = ImageDraw.Draw(self._image)
+
+        self.clear()
+
+    def clear(self):
+        self._draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
+
+    def text(self, position, content):
+        self._draw.text(position, content, font=self.font, fill=255)
+
+    def display(self):
+        self._adafruit.image(self._image)
+        self._adafruit.display()
+
+
 DEVICE_BUS = 1
 DEVICE_ADDR = 0x17
 PROTECT_VOLT = 3700
 SAMPLE_TIME = 2
 
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
 
-# 128x64 display with hardware I2C:
-screen = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
-# Initialize screen
-screen.begin()
-
-screen.clear()
-screen.display()
-
-# Make sure to create image with mode '1' for 1-bit color.
-width = screen.width
-height = screen.height
-image = Image.new('1', (width, height))
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+screen = Screen()
 
 padding = -2
 top = padding
-bottom = height-padding
+bottom = screen.height - padding
 x = 0
 
 # Display counter
 screenC = 0
 
-# Make sure the .ttf font file is in the same directory as the python script!
-font = ImageFont.truetype('PixelOperator.ttf', 16)
+bus = Bus()
+supply = Supply()
+battery = Battery()
 
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    screen.clear()
 
     # Shell scripts for system monitoring from here: https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d\' \' -f1"
@@ -88,60 +101,44 @@ while True:
     cmd = "vcgencmd measure_temp | cut -f 2 -d '='"
     temp = subprocess.check_output(cmd, shell=True)
 
-    # Scripts for UPS monitoring
-    ina = INA219(0.00725, address=0x40)
-    ina.configure()
-    piVolts = round(ina.voltage(), 2)
-    piCurrent = round (ina.current())
-    
-    ina = INA219(0.005, address=0x45) 
-    ina.configure()
-    battVolts = round(ina.voltage(), 2)
-    
-    try:
-        battCur = round(ina.current())
-        battPow = round(ina.power() / 1000, 1)
-    except DeviceRangeError:
+    piVolts = round(supply.voltage(), 2)
+    piCurrent = round (supply.current())
+
+    battVolts = round(battery.voltage(), 2)
+
+    status = battery.status()
+    if status['error'] is None:
+        battCur = round(status['current'])
+        battPow = round(status['power'] / 1000, 1)
+    else:
         battCur = 0
         battPow = 0
-    
-    bus = smbus2.SMBus(DEVICE_BUS)
 
-    aReceiveBuf = bus.read_i2c_block_data(DEVICE_ADDR, 0, 32)
-    
-    if (aReceiveBuf[8] << 8 | aReceiveBuf[7]) > 4000:
-        chargeStat = 'Charging USB C'
-    elif (aReceiveBuf[10] << 8 | aReceiveBuf[9]) > 4000:
-        chargeStat = 'Charging Micro USB.'
-    else:
-        chargeStat = 'Not Charging'
-    
-    battTemp = (aReceiveBuf[12] << 8 | aReceiveBuf[11])
-    battCap = (aReceiveBuf[20] << 8 | aReceiveBuf[19])
+    bus.open()
 
     if (screenC <= 15):
         # Pi Stats Display
-        draw.text((x, top+2), "IP: " + str(IP,'utf-8'), font=font, fill=255)
-        draw.text((x, top+18), str(CPU,'utf-8') + "%", font=font, fill=255)
-        draw.text((x+80, top+18), str(temp,'utf-8'), font=font, fill=255)
-        draw.text((x, top+34), str(MemUsage,'utf-8'), font=font, fill=255)
-        draw.text((x, top+50), str(Disk,'utf-8'), font=font, fill=255)
+        screen.text((x, top+2), "IP: " + str(IP))
+        screen.text((x, top+18), str(CPU) + "%")
+        screen.text((x+80, top+18), str(temp))
+        screen.text((x, top+34), str(MemUsage))
+        screen.text((x, top+50), str(Disk))
 
     else:
         # UPS Stats Display
-        draw.text((x, top+2), "Pi: " + str(piVolts) + "V  " + str(piCurrent) + "mA", font=font, fill=255)
-        draw.text((x, top+18), "Batt: " + str(battVolts) + "V  " + str(battCap) + "%", font=font, fill=255)
+        screen.text((x, top+2), "Pi: " + str(piVolts) + "V  " + str(piCurrent) + "mA")
+        screen.text((x, top+18), "Batt: " + str(battVolts) + "V  " + str(bus.battery_remaining()) + "%")
         if (battCur > 0):
-            draw.text((x, top+34), "Chrg: " + str(battCur) + "mA " + str(battPow) + "W", font=font, fill=255)
+            screen.text((x, top+34), "Chrg: " + str(battCur) + "mA " + str(battPow) + "W")
         else:
-            draw.text((x, top+34), "Dchrg: " + str(0-battCur) + "mA " + str(battPow) + "W", font=font, fill=255)
-        draw.text((x+15, top+50), chargeStat, font=font, fill=255)
+            screen.text((x, top+34), "Dchrg: " + str(0-battCur) + "mA " + str(battPow) + "W")
+        screen.text((x+15, top+50), bus.charging_status())
 
-    screenC+=1
+    bus.close()
+
+    screenC += 1
     if (screenC == 30):
         screenC = 0
 
-    screen.image(image)
     screen.display()
-    bus.close()
     time.sleep(.1)
